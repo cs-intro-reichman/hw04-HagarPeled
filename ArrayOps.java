9653ecd738cbfc806a0ef89def606c2e36d3c5ab
HagarPeled <hagar.peled@post.runi.ac.il>
2024-01-08 19:14:04 +0200

    homework 4

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,16 +1,34 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int[] myArray1= {2, 8, 3, 7, 8};
+        System.out.println(secondMaxValue(myArray1));
+        int[] missingArr= {0, 1, 2, 3, 4, 6};
+        System.out.println(findMissingInt(missingArr)); 
     }
     
-    public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+    public static int findMissingInt (int []  missingArr) {
+    int n = missingArr.length;
+    int sumWithMissingInt = n * (n+1) / 2;
+    int actualSum = 0;
+    for (int i = 0; i < missingArr.length; i++) {
+        actualSum += missingArr[i];
+        }
+    int missingInt = sumWithMissingInt-actualSum;
+    return missingInt;
     }
 
-    public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+    public static int secondMaxValue(int [] myArray1) {
+        int max= myArray1[0];
+        int secondMax= myArray1[1];
+        for (int i = 1; i < myArray1.length; i++) {
+          if (myArray1[i] > max) {
+            secondMax = max;
+            max = myArray1[i];
+    }     else if (myArray1[i] > secondMax) {
+            secondMax = myArray1[i];
+    }
+}
+    return secondMax;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
