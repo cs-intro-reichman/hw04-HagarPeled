46818f9481ee6ac62ceb9331dad957e0ec6a0c5d
HagarPeled <hagar.peled@post.runi.ac.il>
2024-01-10 20:31:04 +0200

    homework 4

--- StringOps.java
+++ StringOps.java
@@ -22,12 +22,30 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+    String string= "One two tHRee world";
+    System.out.println(capVowelsLowRest(string));
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String vowels = "aeiou";
+        String sOut= "";
+        int i=0;
+        
+        while (i  < string.length()) {
+            char currentChar = string.charAt(i);
+            if (vowels.indexOf(currentChar)!= -1) {
+              sOut += (char) (currentChar - 32);
+            } else if ((currentChar >= 97) && (currentChar <= 122)) {
+              sOut += currentChar;  
+            } else if (currentChar == 32) {
+                sOut+= currentChar;
+            } else {
+                sOut += (char) (currentChar + 32);
+            }
+           
+            i++;
+        }
+        return sOut;
     }
 
     public static String camelCase (String string) {
